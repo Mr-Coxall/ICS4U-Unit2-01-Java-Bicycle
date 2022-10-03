a06d6ef53ab0fc6ee4b9eb74fc7887ca26f47045
Patrick Coxall <mr.coxall@mths.ca>
2022-10-03 10:42:08 -0400

    lint fix

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-/**
+/*
  * The Bicycle program implements an application that
  * uses the Bicycle Class.
  *
@@ -7,19 +7,19 @@
  * Since:   2020-01-01
  */
 
- /**
-* This is the standard Main() class starting point.
-*/
+/**
+ * This is the standard Main() class starting point.
+ */
 final class Main {
 
     /**
-    * Prevent instantiation.
-    * Throw an exception IllegalStateException.
-    * if this ever is called
-    *
-    * @throws IllegalStateException
-    *
-    */
+     * Prevent instantiation.
+     * Throw an exception IllegalStateException.
+     * if this ever is called
+     *
+     * @throws IllegalStateException
+     *
+     */
     private Main() {
         throw new IllegalStateException("Cannot be instantiated");
     }
@@ -29,14 +29,14 @@ final class Main {
     *
     * @param args No args will be used
     */
-	public static void main(final String[] args) {
+    public static void main(final String[] args) {
 		// use Bicycle class
 		final Bicycle someBicycleVariable = new Bicycle();
 
 		System.out.println("\nSet speed to 10.");
 		someBicycleVariable.speed = 10;
-    	System.out.println("The current speed is: " + 
-		                    someBicycleVariable.speed);
+    	System.out.println("The current speed is: "
+		                    + someBicycleVariable.speed);
 
     	System.out.println("Apply the brake with value 2.");
     	someBicycleVariable.applyBrake(2);
